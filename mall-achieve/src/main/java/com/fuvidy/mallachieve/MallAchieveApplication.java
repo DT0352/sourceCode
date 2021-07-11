d398dad7f400c9e362ea698f2159cb262bc285a4
fuzhidong <873596356@qq.com>
2021-07-11 18:00:58 +0800

    mybatis generator

---
+++ mall-achieve/src/main/java/com/fuvidy/mallachieve/MallAchieveApplication.java
@@ -0,0 +1,13 @@
+package com.fuvidy.mallachieve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallAchieveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallAchieveApplication.class, args);
+    }
+
+}
