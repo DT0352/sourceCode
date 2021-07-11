57ee5aa75d837cdbc653c7dc431c3c906cdbd451
fuzhidong <873596356@qq.com>
2021-07-11 19:15:52 +0800

    mybatis generator

---
+++ mall-achieve/src/main/java/com/fuvidy/mallachieve/tiny/mbg/config/MyBatisConfig.java
@@ -0,0 +1,14 @@
+package com.fuvidy.mallachieve.tiny.mbg.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @Description
+ * @Author 付志东
+ * @Version V1.0.0
+ * @Since 1.0
+ * @Date 2021/7/11
+ */
+@Configuration("com.fuvidy.mallachieve.tiny.mbg.mapper")
+public class MyBatisConfig {
+}
